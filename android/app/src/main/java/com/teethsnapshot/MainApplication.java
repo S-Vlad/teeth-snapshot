426dd35917296d6f54f375343e5fb9e45674e8bf
Vlad Svidrickiy <v.svidritskiy@wowmaking.net>
2019-05-09 19:06:18 +0300

    Add iphone-x-helper, linear-gradient

--- android/app/src/main/java/com/teethsnapshot/MainApplication.java
+++ android/app/src/main/java/com/teethsnapshot/MainApplication.java
@@ -3,6 +3,7 @@ package com.teethsnapshot;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,7 +26,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
-            new VectorIconsPackage(),
+          new LinearGradientPackage(),
+          new VectorIconsPackage(),
           new RNGestureHandlerPackage()
       );
     }
